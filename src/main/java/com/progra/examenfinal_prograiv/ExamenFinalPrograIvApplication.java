86a4861c308229fab8f4762daf67e05301acfb23
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2021-11-21 12:52:54 -0600

    Initial commit

---
+++ src/main/java/com/progra/examenfinal_prograiv/ExamenFinalPrograIvApplication.java
@@ -0,0 +1,11 @@
+package com.progra.examenfinal_prograiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalPrograIvApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFinalPrograIvApplication.class, args);
+    }
+}
---
+++ src/test/java/com/progra/examenfinal_prograiv/ExamenFinalPrograIvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progra.examenfinal_prograiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalPrograIvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
